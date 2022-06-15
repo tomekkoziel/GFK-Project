8fdaa63a10ca7421c7d9d75aad38c02939649882
tomekkoziel <tomasz.koziel66@gmail.com>
2022-06-15 21:06:05 +0200

    Added sections in mainpage.h

--- include/mainpage.h
+++ include/mainpage.h
@@ -3,57 +3,116 @@
  *
  * $Header: /nfs/slac/g/glast/ground/cvs/workbook/pages/advanced_doxygen/usingDoxygen.htm,v 1.1.1.1 2007/06/29 15:03:16 chuckp Exp $
  */
-/** @mainpage package templates
+/** @mainpage Animacje
  *
- * @authors Documentation Task Force
- *
- * @section intro Introduction
- * This package provides code templates for use by GLAST developers.
- * All header files for external access are located in the templates directory,
- * as it is customary to put external public header files in the packageName
- * directory. Header files that are not meant for external access reside in
- * the src directory. Source files are located in the src directory. Files
- * related to loading sharable libraries are located in the src/Dll directory.
- * There are 3 examples:
- *
- * - User-Defined generic C++ class
- * -# templates/ClassTemplate.h
- * -# src/ClassTemplate.cxx
- * - User-Defined Gaudi Algorithm
- * -# src/ExampleAlg.cxx
- * -# src/Dll/templates_dll.cxx
- * -# src/Dll/templates_load.cxx
- * - User-Defined Gaudi Service
- * -# templates/IExampleSvc.h
- * -# templates/ExampleSvc.h
- * -# src/ExampleSvc.cxx
- * -# src/Dll/templates_dll.cxx
- * -# src/Dll/templates_load.cxx
- *
- *
- * Also note the existence of the following directories:
- * - cmt
- * -# Contains the requirements file
- * - doc
- * -# Contains the release.notes file
- *
- *
- * As you prepare to develop code for GLAST SAS, please be sure you are aware
- * of our current
- * <A HREF="http://www-glast.slac.stanford.edu/software/CodeHowTo/codeStandards.html"> Coding Standards </A>
- *
- *
- * If using the code in this package as an example - please modify the comments
- * as appropriate for your own specific code.
+ * @authors Adam Jędrychowski, Szymon Moździerz, Tomasz Kozieł
  *
  * <hr>
- * @section notes release.notes
+ *
+ * @section description Opis Projektu
+ *
+ * Celem projektu było napisanie programu tworzącego oraz wyświetlającego animację poklatkową złożoną z ciągu bitmap wyświetlanych w zadanych
+ * odstępach czasu. Dane wejściowe pobierane są z pliku tekstowego zawierającego:
+ *
+ * - w pierwszej linii rozmiar generowanych klatek (bitmap)
+ * - w drugiej linii ilość wszystkich klatek (w celu przyspieszenia procesu zapisywania danych w std::vector)
+ * - numer klatki oraz czas jej trwania w milisekundach
+ * - opis zawartości każdej z klatek, zapisany jako szereg instrukcji rysujących (każda w oddzielnej linii), użyte instrukcje (w poniższych instrukcjach znacznik flaga informuje o tym czy figura ma być wypełniona kolorem pędzla (flaga=1) czy też program ma narysować jedynie obrys (flaga=0)):
+ *   - punkt (PT) x, y – rysuje punkt o współrzędnych x i y
+ *   - elipsa (EL) x1, y1, x2, y2, flaga – rysuje elipsę
+ *   - prostokat (PR) x1, y1, x2, y2, flaga – rysuje prostokąt
+ *   - linia (LN) x1, y1, x2, y2 – rysuje linie
+ *   - rozmiar_piora (RP) d – ustala grubość pióra
+ *   - kolor_piora (KP) r g b – ustawia kolor pióra
+ *   - kolor_wypelnienia (KW) r g b - ustawia kolor wypełnienia (pędzla)
+ * - końca klatki sygnalizowanego instrukcją stop (ST)
+ *
+ *
+//  * There are 3 examples:
+//  *
+//  * - User-Defined generic C++ class
+//  * -# templates/ClassTemplate.h
+//  * -# src/ClassTemplate.cxx
+//  * - User-Defined Gaudi Algorithm
+//  * -# src/ExampleAlg.cxx
+//  * -# src/Dll/templates_dll.cxx
+//  * -# src/Dll/templates_load.cxx
+//  * - User-Defined Gaudi Service
+//  * -# templates/IExampleSvc.h
+//  * -# templates/ExampleSvc.h
+//  * -# src/ExampleSvc.cxx
+//  * -# src/Dll/templates_dll.cxx
+//  * -# src/Dll/templates_load.cxx
+//  *
+//  *
+//  * Also note the existence of the following directories:
+//  * - cmt
+//  * -# Contains the requirements file
+//  * - doc
+//  * -# Contains the release.notes file
+//  *
+//  * As you prepare to develop code for GLAST SAS, please be sure you are aware
+//  * of our current
+//  * <A HREF="http://www-glast.slac.stanford.edu/software/CodeHowTo/codeStandards.html"> Coding Standards </A>
+//  *
+//  *
+//  * If using the code in this package as an example - please modify the comments
+//  * as appropriate for your own specific code.
+ *
+ * <hr>
+ * @section head_of_terms Założenia wstępne przyjęte w realizacji projektu
  * release.notes
+ *
  * <hr>
- * @section requirements requirements
- * @verbinclude requirements
+ *
+ * @section analysis Analiza projektu
+ *
+ *
+ *
+ *
+ *
  * <hr>
- * @todo [optionally include text about more work to be done]
- * @todo Give each todo item its own line
+ *
+ * @section work_division Podział pracy i znaliza czasowa
+ *
+ *
+ *
+ *
+ *
+ *
+ *  <hr>
+ *
+ * @section algorithms Opracowanie i opis niezbędnych algorytmów
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *  <hr>
+ *
+ * @section tests Testowanie
+ *
+ *
+ *
+ *
+ *
+ * <hr>
+ *
+ * @section conclusion Wdrożenie, raport i wnioski
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+//  * @section requirements requirements
+//  * @verbinclude requirements
+//  * <hr>
+//  * @todo [optionally include text about more work to be done]
+//  * @todo Give each todo item its own line
  *
  */
